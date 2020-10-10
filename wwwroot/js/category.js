13141f6521295b82434d9d23d8157cce632e7bd1
ucadena07 <ulises@netkinetix.com>
2020-10-10 11:09:38 -0500

    upsert create/delete/swal

--- wwwroot/js/category.js
+++ wwwroot/js/category.js
@@ -1,4 +1,5 @@
-﻿var dataTable;
+﻿
+var dataTable;
 
 $(document).ready(function () {
     loadDataTable();
@@ -19,7 +20,7 @@ function loadDataTable() {
                                 <a href="/Admin/Category/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer">
                                     <i class="fas fa-edit"></i>
                                 </a>
-                                <a  class="btn btn-danger text-white" style="cursor:pointer">
+                                <a onclick=Delete("/Admin/Category/Delete/${data}")  class="btn btn-danger text-white" style="cursor:pointer">
                                     <i class="fas fa-trash-alt"></i>
                                 </a>
                             </div>
@@ -28,5 +29,32 @@ function loadDataTable() {
                 }, "width": "40"
             }
         ]
+    });
+}
+
+function Delete(url) {
+    swal({
+        title: "Are you sure you want to Delete?",
+        text: "You will not be able to restore the data!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+
+                }
+            });
+        }
     });
 }
